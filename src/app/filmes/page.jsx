63db05ad3dd3f5a143a8bc52a3c5c1509f56ba5e
SilasP0 <possarle.silas16@gmail.com>
2026-09-23 14:33:34 -0300

    First Commit

---
+++ src/app/filmes/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function Filmes() {
+    const [listaFilmes, setListaFilmes] = useState([]);
+
+    return (
+        <main>
+            
+        </main>
+    )
+}
